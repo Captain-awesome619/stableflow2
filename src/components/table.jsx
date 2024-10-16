6e1e4d5dd8b29cb20a78e9ebcfbfe55910b7e64a
unknown <ogunsolatoluwalase@gmail.com>
2024-10-17 00:22:38 +0100

    table and keys

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -6,6 +6,7 @@ const DataTable = () => {
   const data = React.useMemo(
     () => [
       {
+        id: 1,
         description: 'Transaction 1',
         date: '2024-10-12',
         type: 'Credit',
@@ -13,6 +14,7 @@ const DataTable = () => {
         status: 'Completed',
       },
       {
+        id: 2,
         description: 'Transaction 2',
         date: '2024-10-11',
         type: 'Debit',
@@ -20,21 +22,24 @@ const DataTable = () => {
         status: 'Pending',
       },
       {
-        description: 'Transaction 2',
+        id: 3,
+        description: 'Transaction 3',
         date: '2024-10-11',
         type: 'Debit',
         amount: 50,
         status: 'Pending',
       },
       {
-        description: 'Transaction 2',
+        id: 4,
+        description: 'Transaction 4',
         date: '2024-10-11',
         type: 'Debit',
         amount: 50,
         status: 'Pending',
       },
       {
-        description: 'Transaction 2',
+        id: 5,
+        description: 'Transaction 5',
         date: '2024-10-11',
         type: 'Debit',
         amount: 50,
@@ -87,11 +92,12 @@ const DataTable = () => {
   return (
     <table {...getTableProps()} className="min-w-full border-collapse border ">
       <thead className="bg-gray-100">
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+            {headerGroup.headers.map((column) => (
               <th
                 {...column.getHeaderProps()}
+                key={column.id}
                 className={`border border-gray-300 p-2 ${column.align === 'right' ? 'text-right' : 'text-left'}`}
               >
                 {column.render('Header')}
@@ -101,14 +107,15 @@ const DataTable = () => {
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
+        {rows.map((row) => {
           prepareRow(row);
           return (
-            <tr {...row.getRowProps()} className="even:bg-gray-50">
-              {row.cells.map(cell => (
+            <tr {...row.getRowProps()} key={row.id} className="even:bg-gray-50">
+              {row.cells.map((cell) => (
                 <td
                   {...cell.getCellProps()}
-                  className={`border  p-2 ${cell.column.align === 'right' ? 'text-right' : 'text-left'}`}
+                  key={cell.column.id}
+                  className={`border p-2 ${cell.column.align === 'right' ? 'text-right' : 'text-left'}`}
                 >
                   {cell.render('Cell')}
                 </td>
