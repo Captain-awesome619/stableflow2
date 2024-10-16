e51bdb66aa82fc9bd6914e167d2416878fc79b2e
unknown <ogunsolatoluwalase@gmail.com>
2024-10-17 00:38:13 +0100

    ui

--- src/components/steps.jsx
+++ src/components/steps.jsx
@@ -8,7 +8,7 @@ const Steps = () => {
   <h1 className="flex  justify-center pt-[1rem] text-[px] lg:text-[45px] text-center h-[200px] lg:h-[400px] w-screen font-[700] text-primary1">Three Steps to Run <br></br> Your Business Finances Easily.</h1>
   </div>
   <div className=" lg:mt-[-200px] mt-[-100px]">
-    <Image src={step} width={1000} height={1000} alt="Three Steps Image" className=" lg:w-[1000px] lg:h-[500px] w-[350px] h-[200px] " />
+    <Image src={step} width={1000} height={1000} alt="Three Steps Image" className=" lg:w-[1000px] lg:h-[500px] w-[430px] h-[200px] " />
   </div>
 </div>
 
